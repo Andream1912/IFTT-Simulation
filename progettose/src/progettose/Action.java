102bb8512c9cb54e0227cac0b25967c618ecfee4
SmuelIsHere <s.l.y.3@hotmail.it>
2023-11-20 23:21:25 +0100

    Implementation of Action interface

---
+++ progettose/src/progettose/Action.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package progettose;
+
+/**
+ *
+ * @author smuel
+ */
+public interface Action {
+    
+    public void execute(); //void method for executing action
+    
+}
