a7a47167972d8a2a21742c181b020d9436362610
Andrea <aweyxzombie@gmail.com>
2023-11-21 21:33:51 +0100

    fix testing files

--- progettose/test/TriggerTest.java
+++ progettose/test/TriggerTest.java
@@ -3,20 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 import java.time.LocalTime;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import progettose.triggerPackage.TimeTrigger;
 import progettose.triggerPackage.TimeTriggerCreator;
 import progettose.triggerPackage.Trigger;
 
 
 public class TriggerTest {
+    
+    
+    @Test
+    public void testTimeTriggerExecution() {
+        // Creazione di un'istanza di TimeTrigger con un orario specifico
+        LocalTime time = LocalTime.of(12, 30);
+        
+        TimeTrigger timeTrigger = new TimeTrigger(time);
+
+        // Esecuzione del trigger
+        timeTrigger.evaluate();
+
+        // Puoi aggiungere ulteriori asserzioni se necessario
+    }
     @Test
     public void testTimeTriggerCreator(){
         //Creating an instance of TimeTriggerCreator with a specific time stamp
-        LocalTime triggerTime = LocalTime.of(12, 30);
-        TimeTriggerCreator timeTriggerCreator = new TimeTriggerCreator(triggerTime);
+        LocalTime time = LocalTime.of(12, 30);
+        TimeTriggerCreator timeTriggerCreator = new TimeTriggerCreator(time);
         
         //Trigger creation using the creator
         Trigger trigger = timeTriggerCreator.createTrigger();
