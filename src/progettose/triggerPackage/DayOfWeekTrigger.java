761af87d41bad4418d7515b1ac505eb6a154b994
McolKEVLAR <manuel.monastero@live.it>
2023-11-29 17:40:17 +0100

    dayofweek trigger added and tested

---
+++ src/progettose/triggerPackage/DayOfWeekTrigger.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progettose.triggerPackage;
+
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+
+/**
+ *
+ * @author manue
+ */
+public class DayOfWeekTrigger implements Trigger{
+    private String day;
+    
+    public DayOfWeekTrigger(String day) {
+        this.day=day; 
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    @Override
+    public boolean evaluate() {
+        DayOfWeek currentDay = LocalDate.now().getDayOfWeek();
+        return DayOfWeek.valueOf(day)==currentDay;
+    }
+    
+    @Override
+    public String toString() {
+        return "On " + day;
+    }
+    
+}
